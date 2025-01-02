0a9ccbda8130b0aca98268ff5432624fcab24d9d
jagadw <jagadhitawicaksana@gmail.com>
2025-01-02 15:08:50 +0800

    add component: content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,21 @@
 <script>
     import Header from "./header.svelte";
+    import Content from "./content.svelte";
+    let title = "Web Host";
+    let desc = "Dapatkan kebutuhan websitemu hanya di ";
 </script>
 
 <Header/>
 
+<br>
+    <center>
+        <h1>{title}</h1>
+        <h3>{desc}{title}!</h3>
+    </center>
+<br>
+
+<Content/>
+
 <style>
-    h1 {
-        color: aqua;
-    }
+
 </style>
