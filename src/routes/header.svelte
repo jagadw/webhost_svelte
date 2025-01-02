08fa09e18896f091d827a05d33221b9c748d435c
jagadw <jagadhitawicaksana@gmail.com>
2025-01-02 11:19:35 +0800

    add component header

---
+++ src/routes/header.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<header>
+    <center>
+        <b>Start your free-trial today!</b>
+        <button>Get Started</button>
+        <button>Learn More</button>
+    </center>
+</header>
+
+<style>
+    
+</style>
